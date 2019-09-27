8c3b622ec1c43f7b17c66a003aa23ebcfeaad8a3
hassnian <hassnianxd@gmail.com>
2019-09-28 01:46:17 +0200

    merge

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Alert from "@kiwicom/orbit-components/lib/Alert";
+
+
+const Home = () => (
+  <div>
+    <Alert >asdasd</Alert>
+  </div>
+);
+
+export default Home;
---
+++ frontend/src/router/routes.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import uuid from "uuid/v4";
+import Home from "../pages/Home/Home";
+import { ProtectedRoute } from "./ProtectedRoute";
+
+const routes = [
+  {
+    path: "/home",
+    exact: true,
+    auth: true,
+    component: () => <Home />,
+  }, {
+    path: "*",
+    exact: true,
+    auth: false,
+    component: () => <h1>404 not found</h1>,
+  },
+];
+
+
+const makeRoutes = () => routes.map((route) => {
+  if (route.auth) {
+    return <ProtectedRoute key={uuid()} {...route} />;
+  }
+  return <Route key={uuid()} {...route} />;
+});
+
+
+export default makeRoutes();
