3b16132504a46992928eb01b25c34626020ba82a
El_Piola <leandrovejar@gmail.com>
2019-09-28 01:32:42 +0200

    Auto stash before merge of "develop" and "origin/develop"

---
+++ Backend/database/migrations/2019_09_27_210844_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('event_name');
+            $table->integer('number_volunteers');
+            $table->string('location');
+            $table->dateTime('event_time');
+            $table->string('description');
+            $table->string('interests');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
